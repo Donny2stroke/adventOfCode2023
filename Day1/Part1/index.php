84ce681c6f31c236e4e20679297ab036b2517b2a
Claudio <claudio2000@hotmail.it>
2023-12-01 15:55:31 +0100

    Day1

--- Day1/Part1/index.php
+++ Day1/Part1/index.php
@@ -1,1 +1,51 @@
-ciao
+<?php
+$righeFile = array();
+function caricaFile($nomeFile){
+    $myfile = fopen(__DIR__ . "/".$nomeFile, "r") or die("Unable to open file!");
+    // Output one character until end-of-file
+    while(!feof($myfile)) {
+        $rigaFile = fgets($myfile);
+        array_push($GLOBALS['righeFile'],$rigaFile);
+    }
+    fclose($myfile);
+}
+caricaFile("input.txt");
+
+
+$somma = 0;
+foreach($righeFile as $riga){
+    $arrayNumeri = array();
+    preg_match_all('/[0-9]/', $riga, $arrayNumeri); 
+
+    $primoValore = $arrayNumeri[0][0];
+    $ultimoValore = $arrayNumeri[0][count($arrayNumeri[0])-1];
+    $valore = ($primoValore*10)+$ultimoValore;
+    $somma = $somma + $valore;
+
+
+
+    echo $riga;
+    echo "Primo Valore: ".$primoValore.PHP_EOL;
+    echo "Ultimo Valore: ".$ultimoValore.PHP_EOL;
+    echo "Unico numero: ".$valore.PHP_EOL;
+    echo "Somma: ".$somma.PHP_EOL;
+
+    //echo $valore.PHP_EOL;
+
+    /* print_r($arrayNumeri);
+    echo PHP_EOL;
+    echo "Primo Valore: ".$primoValore.PHP_EOL;
+    echo "Ultimo Valore: ".$ultimoValore.PHP_EOL; */
+
+    //die();
+}
+
+echo $somma;
+
+
+
+
+
+
+
+?>
