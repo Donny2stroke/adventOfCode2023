acaaa88780024bc229227c9de797f5b24a9d989f
Claudio D'Onofrio <claudio2000@hotmail.it>
2023-12-02 12:35:20 +0100

    chiusura day 1 part 1

--- Day1/Part1/index.php
+++ Day1/Part1/index.php
@@ -4,7 +4,7 @@ function caricaFile($nomeFile){
     $myfile = fopen(__DIR__ . "/".$nomeFile, "r") or die("Unable to open file!");
     // Output one character until end-of-file
     while(!feof($myfile)) {
-        $rigaFile = fgets($myfile);
+        $rigaFile = trim(fgets($myfile));
         array_push($GLOBALS['righeFile'],$rigaFile);
     }
     fclose($myfile);
@@ -21,23 +21,6 @@ foreach($righeFile as $riga){
     $ultimoValore = $arrayNumeri[0][count($arrayNumeri[0])-1];
     $valore = ($primoValore*10)+$ultimoValore;
     $somma = $somma + $valore;
-
-
-
-    echo $riga;
-    echo "Primo Valore: ".$primoValore.PHP_EOL;
-    echo "Ultimo Valore: ".$ultimoValore.PHP_EOL;
-    echo "Unico numero: ".$valore.PHP_EOL;
-    echo "Somma: ".$somma.PHP_EOL;
-
-    //echo $valore.PHP_EOL;
-
-    /* print_r($arrayNumeri);
-    echo PHP_EOL;
-    echo "Primo Valore: ".$primoValore.PHP_EOL;
-    echo "Ultimo Valore: ".$ultimoValore.PHP_EOL; */
-
-    //die();
 }
 
 echo $somma;
